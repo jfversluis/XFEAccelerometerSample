e7df7e903608105686418c64e5bc5b559f460258
Gerald Versluis <gerald@verslu.is>
2020-10-05 09:17:11 +0200

    Implement Accelerometer

--- XFEAccelerometerSample/MainPage.xaml.cs
+++ XFEAccelerometerSample/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFEAccelerometerSample
@@ -13,6 +14,34 @@ namespace XFEAccelerometerSample
         public MainPage()
         {
             InitializeComponent();
+
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        }
+
+        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs args)
+        {
+            xResult.Text = $"X: {args.Reading.Acceleration.X}";
+            yResult.Text = $"Y: {args.Reading.Acceleration.Y}";
+            zResult.Text = $"Z: {args.Reading.Acceleration.Z}";
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                    Accelerometer.Stop();
+                else
+                    Accelerometer.Start(SensorSpeed.UI);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Something else went wrong
+            }
         }
     }
 }
